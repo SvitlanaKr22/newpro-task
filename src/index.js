4880868c033613fb9e118765a8b90d29b7f864f2
Svitlana Kravets <svetakravez72@gmail.com>
2023-07-11 15:36:21 +0300

    files composition

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
-import "./index.css";
-import App from "./components/App/App";
-import { BrowserRouter } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import './index.css';
+import App from './components/App/App';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
   <Provider store={store}>
     <PersistGate persistor={persistor} loading={null}>
-      <BrowserRouter basename="/project-MyErrand08">
+      {/* <BrowserRouter basename="/project-MyErrand08"> */}
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </PersistGate>
